c739cce32e4e0f55ff444a7c5f3718140916fc63
jonrodriguez1 <jonathrr@usc.edu>
2017-04-16 16:15:49 -0700

    Add files via upload

--- main.cu
+++ main.cu
@@ -9,120 +9,150 @@
 #include <cmath>
 #include <cublas.h>
 
+// for now
+
 using namespace std;
 
 // standard, defined variables
-#define output_file "output.png"	// output image file
+#define output_file "output_image.png"	// output image file
 
-// one-dimensional mean filter, which goes through the input image and releases an output
-// takes three inputs: input array, output array & size number
-void average_filter_1D(double *input, double *output, int size) {
-	// move the window through all elements of the input
-	// assumes a window of size 5 for now.
-	int i;
-	for (i = 2; i < size - 2; i++) {
-		output[i - 2] = (input[i - 2] + input[i - 1] + input[i] + input[i + 1] + input[i + 2]) / 5;
-	}
-}
-
-// two-dimensional averga efilter
-// takes in an array and gives in the output
-// this function also requires the height and width of the image
-void average_filter_2D(double *input, double *output, int height, int width) {
-	// go through all elements of the image
-	int m, n;
-	for (m = 1; m < height - 1; ++m) {
-		for (n = 1; n < width - 1; ++n) {
-			// take the average
-			output[(m - 1) * (width - 2) + n - 1] = (
-				input[(m - 1) * width + n - 1] * 
-				input[(m - 1) * width + n] * 
-				input[(m - 1) * width + n + 1] * 
-				input[m * width + n - 1] * 
-				input[m * width + n] * 
-				input[m * width + n + 1] * 
-				input[(m + 1) * width + n - 1] * 
-				input[(m + 1) * width + n] *
-				input[(m + 1) * width + n + 1]) / 9;
-		}
-	}
-}
-
-// second kernel, which performs the low-pass filter 
-// 2D array should be initialized as a result of the previous Fourier transform
-// let's begin with a serial implementation, for now
-void gauss_filter(double *in) {
-	// first step: converting from a 1D linear array into a 2D matrix
-	double arr_temp[1024][1024];
-	int x, y;
-	for (x = 0; x < h; x++) {
-		for (y = 0; y < w; y++) {
-			arr_temp[x][y] = in[x * 1024 + y];
-		}
-	}
-
-	// assigning standard deviation values
-	double std_dev = 1.0;
-	double r = 2.0 * std_dev * std_dev;
-	double s = 2.0 * std_dev * std_dev;
-
-	// initialize the sum for normalization
-	double sum = 0.0;
-
-	// creating offset variables
-	int offset_x = h / 2;
-	int offset_y = w / 2;
-
-	// loop to generate a kernel, similar in size to the original image
-	for (x = -offset_x; x <= offset_x; x++) {
-		for (y = -offset_y; y <= offset_y; y++) {
-			double x_p = (double) x;
-			double y_p = (double) y;
-			r = sqrt((x_p * x_p) + (y_p * y_p));
-			arr_temp[x + 1024][y + 1024] = (exp(-(r * r) / s)) / (M_PI * s);
-			sum += arr_temp[x + 1024][y + 1024];
-		}
-	}
-
-	// loop again to normalize the loop
-	for (x = 0; x < h; x++) {
-		for (y = 0; y < w; y++) {
-			arr_temp[x][y] = arr_temp[x][y] / sum;
-		}
-	}
-
-	// final step is to convert from 2D array to 1D row-major linear array
-	for (x = 0; x < h; x++) {
-		for (y = 0; y < w; y++) {
-			in[x * 1024 + y] = arr_temp[x][y];
-		}
-	}
-}
-
-// first CUDA kernel, which performs Fourier transform on the image
+// CUDA kernel, which performs Fourier transform on the image
 // while this code loops in n^2 time, CUDA parallelism should make the program faster
-__global__ void fourier_transform(double *in, double *out, int height, int width, int blockConfig) {
-	// block elements
+// only real values would be considered, as the following is extracted from the image
+__global__ void fourier_transform(float *in, float *out, int height, int width, int blockConfig) {
+	// block elements and function variables
 	int my_x, k, t;
 	my_x = blockIdx.x * blockDim.x + threadIdx.x;
 
 	// iterate through each element, going from frequency to time domain
 	for (k = 0; k < height; k++) {
 		// difference, which will be used to subtract off
-		double realSum = 0;
+		float realSum = 0.0;
 		// iterate through the input element
 		for (t = 0; t < width; t++) {
-			double angle = 2 * M_PI * (my_x * height + t) * (my_x * height + k) / height;
+			// calculate the angle and update the sum
+			float angle = 2 * M_PI * (my_x * height + t) * (my_x * height + k) / height;
 			realSum += in[my_x * height + t] * cos(angle);
 		}
+		// each output element will be the current sum for that index
 		out[my_x * height + k] = realSum;
 	}
 }
 
+// another function, which is responsible for performing 2D convolution on the image
+// to continue with the process, the image should be in the frequency domain
+void image_convolution(float *input, float *output, int height, int width, int kern_size) {
+	// first step will be mapping the image array
+	// this involves mapping from a 1D linear array to a 2D mesh array
+	int x, y, i, j;		// index variables
+
+	// temporary float and kernel array, used to store the 2D mesh version
+	float **temp_array;
+	float **temp_kernel;
+	temp_array = (float**) malloc(sizeof(float*) * width);
+	temp_kernel = (float**) malloc(sizeof(float*) * kern_size);
+
+	// mapping out 2D float array
+	for (x = 0; x < height; x++) {
+		temp_array[x] = (float*) malloc(sizeof(float) * height);
+	}
+
+	// mapping out 2D kernel array
+	for (x = 0; x < kern_size; x++) {
+		temp_kernel[x] = (float*) malloc(sizeof(float) * kern_size);
+	}
+
+	// mapping from 1D to 2D
+	for (x = 0; x < height; x++) {
+		for (y = 0; y < width; y++) {
+			temp_array[x][y] = input[x * height + y];
+		}
+	}
+
+	// filling up the kernel mask with values, where the sum of all elements equals to 1
+	for (x = 0; x < kern_size; x++) {
+		for (y = 0; y < kern_size; y++) {
+			temp_kernel[x][y] = 1 / (kern_size * kern_size);
+		}
+	}
+
+	// offset variable, to determine sizing
+	int kern_offset = kern_size / 2;
+
+	// performing the actual convolution procedure
+	// each element doesn't start at 0, as it won't risk from out of bounds calculations
+	// the true starting point depends on the offset, which is based on the kernel size
+	for (x = kern_offset; x < height - kern_offset; x++) {
+		for (y = kern_offset; y < width - kern_offset; y++) {
+			// initialize accumulated value (unweighted sum) and weighted sum
+			float addValue = 0;
+			float weightedSum = 0;
+
+			// the next series of loops will iterate through the mask
+			for (i = -kern_offset; i <= kern_offset; i++) {
+				for (j = -kern_offset; j <= kern_offset; j++) {
+					// updating weighted sum values
+					float posValue = temp_array[x + i][y + j];
+					addValue += posValue * temp_kernel[kern_offset + i][kern_offset + j];
+					weightedSum += temp_kernel[kern_offset + i][kern_offset + j];
+				}
+			}
+
+			// updating each element of the larger array
+			temp_array[x][y] = (addValue / weightedSum);
+		}
+	}
+
+	// printing out the output array, based on filled values
+	for (x = 0; x < height; x++) {
+		for (y = 0; y < width; y++) {
+			output[x * height + y] = temp_array[x][y];
+		}
+	}
+
+	// deallocate 2D kernel array
+	for (x = 0; x < kern_size; x++) {
+		free(temp_kernel[x]);
+	}
+
+	// deallocating the 2D array, once finished using it
+	for (x = 0; x < height; x++) {
+		free(temp_array[x]);
+	}
+
+	free(temp_kernel);
+	free(temp_array);
+}
+
+// ------------------- necessary functions for convolution filter -------------------//
+// image reflection function
+// any pixel lying outside of the image would be reflected back to the image
+int reflect(int width, int pixel) {
+	if (pixel < 0) {
+		return -pixel - 1;
+	}
+	if (pixel >= width) {
+		return 2 * width - pixel - 1;
+	}
+	return pixel;
+}
+
+// circular indexing function
+// any coordinates that exceeds bounds would be wrapped to the opposite side
+int circular(int width, int pixel) {
+	if (pixel < 0) {
+		return pixel + width;
+	}
+	if (pixel >= width) {
+		return pixel - width;
+	}
+	return pixel;
+}
+
+
 // third kernel, which does the inverse Fourier transform
 // as this only involves real numbers, it's basically an averaged Fourier transform, after all mods
-__global__ void inverse_transform(double *in, double *out, int height, int width) {
-
+__global__ void inverse_transform(float *in, float *out, int height, int width) {
 	// block elements
 	int my_x, k, t;
 	my_x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -130,10 +160,10 @@ __global__ void inverse_transform(double *in, double *out, int height, int width
 	// iterate through each element, going from frequency to time domain
 	for (k = 0; k < height; k++) {
 		// difference, which will be used to subtract off
-		double realSum = 0;
+		float realSum = 0;
 		// iterate through the input element
 		for (t = 0; t < width; t++) {
-			double angle = 2 * M_PI * (my_x * height + t) * (my_x * height + k) / height;
+			float angle = 2 * M_PI * (my_x * height + t) * (my_x * height + k) / height;
 			realSum += in[my_x * height + t] * cos(angle);
 		}
 		out[my_x * height + k] = (realSum / height);
@@ -148,18 +178,19 @@ __global__ void inverse_transform(double *in, double *out, int height, int width
 // ****** 0 = 512x512	---> block image
 // ****** 1 = 1024x1024	---> 720p image
 // ****** 2 = 2048x2048	---> 900p image
+// argv[4] = convolution mask width
 int main(int argc, char **argv) {
 	// height and width variables, critical for this program
 	int height, width;
 
 	// checks for sufficient amount of arguments
-	if (argc != 4) {
+	if (argc != 5) {
 		printf("Invalid amount of arguments\n");
 		return 1;
 	}
 
 	// checks whether a valid image mode is set
-	if ((argv[3] > 2) || (argv[4] < 0)) {
+	if ((atoi(argv[3]) > 2) || (atoi(argv[3]) < 0)) {
 		printf("Invalid image mode set\n");
 		return 1;
 	}
@@ -167,29 +198,29 @@ int main(int argc, char **argv) {
 	// updates image array height and width, based on image mode selected
 	// for now, square images will be tested
 	// initiate 512x512 image
-	if (argv[0] == 0) {
+	if (atoi(argv[0]) == 0) {
 		height = 512;
 		width = 512;
 	}
 
 	// initiate 1024x1024 image
-	else if (argv[0] == 1) {
+	else if (atoi(argv[0]) == 1) {
 		height = 1024;
 		width = 1024;
 	}
 
 	// initiate 2048x2048 image
-	else if (argv[0] == 2) {
+	else if (atoi(argv[0]) == 2) {
 		height = 2048;
 		width = 2048;
 	}
 
-	int i, j;	// index variables, which will iterate through the image
+	int i, j;	// index variables, which will iterate through loops
 	FILE *fp;	// file container
 
 	// variables for time measurement
 	struct timespec start, stop;
-	double time;
+	float time;
 
 	// generating dynamic arrays 
 	// these arrays will be allocated in row-major order: a[i, j] = a[i * 1024 + j]
@@ -197,16 +228,19 @@ int main(int argc, char **argv) {
 	unsigned char *b = (unsigned char*) malloc(sizeof(unsigned char) * height * width);	// end array
 
 	// temporary double variables
-	double *a_temp = (double*) malloc(sizeof(double) * height * width);
-	double *b_temp = (double*) malloc(sizeof(double) * height * width);
-	double *c_temp = (double*) malloc(sizeof(double) * height * width);
+	float *a_temp = (float*) malloc(sizeof(float) * height * width);
+	float *b_temp = (float*) malloc(sizeof(float) * height * width);
+	float *c_temp = (float*) malloc(sizeof(float) * height * width);
 
 	// dynamically allocating GPU (device) arrays
 	// these arrays will interface directly with global functions, listed above
-	double *gpu_a, *gpu_b, *gpu_c;
-	cudaMalloc((void**) &gpu_a, sizeof(double) * height * width);
-	cudaMalloc((void**) &gpu_b, sizeof(double) * height * width);
-	cudaMalloc((void**) &gpu_c, sizeof(double) * height * width);
+	float *gpu_a, *gpu_b, *gpu_c;
+	cudaMalloc((void**) &gpu_a, sizeof(float) * height * width);
+	cudaMalloc((void**) &gpu_b, sizeof(float) * height * width);
+	cudaMalloc((void**) &gpu_c, sizeof(float) * height * width);
+
+	// print statement, ensuring all the memory has been allocated
+	printf("Dynamic memory allocation complete\n");
 
 	// check whether the file can be opened or not
 	// basically, it checks if the file is in the current directory
@@ -216,13 +250,13 @@ int main(int argc, char **argv) {
 	}
 
 	// reading file contents and copy onto array a
-	fread(a, sizeof(unsigned char), w * h, fp);
+	fread(a, sizeof(unsigned char), width * height, fp);
 	fclose(fp);
 
 	// copy to temporary array, convert unsigned char to double
 	for (i = 0; i < height; i++) {
 		for (j = 0; j < width; j++) {
-			a_temp[i * height + j] = (double) a[i * height + j];
+			a_temp[i * height + j] = (float) a[i * height + j];
 		}
 	}
 
@@ -231,37 +265,38 @@ int main(int argc, char **argv) {
 
 	// setting up the block and grid configurations
 	dim3 dimBlock(numThreads);
-	dim3 dimGrid(1024 / numThreads);
+	dim3 dimGrid(height / numThreads);
 
 	// copying host onto the device
-	cudaMemcpy(gpu_a, a_temp, sizeof(double) * height * width, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_a, a_temp, sizeof(float) * height * width, cudaMemcpyHostToDevice);
 
 	// measuring the start time
 	if (clock_gettime(CLOCK_REALTIME, &start) == -1) {
 		perror("Clock gettime");
 	}
 
-	// the CUDA processes will go here
-	// first step is to divide the image into its RGB values
+	// --------------------- the CUDA processes will go here --------------------------------//
+	// perform a Fourier transform on the resulting images
+	fourier_transform<<<dimGrid, dimBlock>>> (gpu_a, gpu_b, height, width, (height / numThreads));
+	cudaMemcpy(b_temp, gpu_b, sizeof(float) * height * width, cudaMemcpyDeviceToHost);
 
+	// perform a 2D image convolution on the image
+	// keep in mind, the image should be in the frequency domain first
+	image_convolution(b_temp, c_temp, height, width, atoi(argv[4]));
 
-	// second step: perform a Fourier transform on the resulting images
-	fourier_transform<<<dimGrid, dimBlock>>> (gpu_a, gpu_b, height, width, (1024 / numThreads));
+	// copy the results of the c_temp array onto its respective GPU array
+	cudaMemcpy(c_temp, gpu_c, sizeof(float) * height * width, cudaMemcpyHostToDevice);
 
-	// third step: apply the mean, or average, filter on the image in the frequency domain
-	// this will involve performing the filter on the second GPU
-
-	// fourth step: compress the three arrays (RGB) back into the single array
-
-	// final step: performing the inverse Fourier transform and obtaining the output
-	//inverse_transform<<<dimGrid, dimBlock>>> (gpu_c, gpu_a);
-	//cudaMemcpy(gpu_a, a_temp, sizeof(double) * h * w, cudaMemcpyDeviceToHost);
+	// performing the inverse Fourier transform to obtain the final image
+	inverse_transform<<<dimGrid, dimBlock>>> (gpu_c, gpu_a, height, width);
+	cudaMemcpy(gpu_a, a_temp, sizeof(double) * height * width, cudaMemcpyDeviceToHost);
+	//--------------------------------------------------------------------------------------//
 
 	// measure the end time here
 	if (clock_gettime(CLOCK_REALTIME, &stop) == -1) {
 		perror("Clock gettime");
 	}
-	time = (stop.tv_sec - start.tv_sec) + (double) (stop.tv_nsec - start.tv_nsec) / 1e9;
+	time = (stop.tv_sec - start.tv_sec) + (float) (stop.tv_nsec - start.tv_nsec) / 1e9;
 
 
 	// print out the execution time here
@@ -295,5 +330,9 @@ int main(int argc, char **argv) {
 	cudaFree(gpu_a); 
 	cudaFree(gpu_b); 
 	cudaFree(gpu_c);
+
+	// print statement, ensuring all memory is freed
+	printf("Memory dump complete\n");
+
 	return 0;
 }
