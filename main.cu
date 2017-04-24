8c9418643e7dcba02639b00688e52adfc0e5d852
jonrodriguez1 <jonathrr@usc.edu>
2017-04-24 08:15:38 -0700

    Add files via upload

--- main.cu
+++ main.cu
@@ -30,7 +30,7 @@ __global__ void fourier_transform(float *in, float *out, int height, int width, 
 		// iterate through the input element
 		for (t = 0; t < width; t++) {
 			// calculate the angle and update the sum
-			float angle = 2 * M_PI * (my_x * height + t) * (my_x * height + k) / height;
+			float angle = 2 * M_PI * (my_x * height + t) * (my_x * width + k) / height;
 			realSum += in[my_x * height + t] * cos(angle);
 		}
 		// each output element will be the current sum for that index
